f152315e64d229a9d8e362a6fb2e2a312a7648f0
ismailrisky <ismailrisky01@gmail.com>
2021-11-24 10:14:22 +0700

    test

--- app/src/main/java/com/example/latansa_book/LoginFragment.kt
+++ app/src/main/java/com/example/latansa_book/LoginFragment.kt
@@ -10,6 +10,7 @@ import android.view.ViewGroup
 class LoginFragment : Fragment() {
 
 //
+    //
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
